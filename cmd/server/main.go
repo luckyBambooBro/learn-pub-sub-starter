d8a66edd56c32ea104844bb27d49c92c7790013b
Huy Pham <huypham008@hotmail.com>
2026-07-31 21:11:17 +0930

    repush

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -16,10 +18,18 @@ func main() {
 	connStr := "amqp://guest:guest@localhost:5672/"
 	conn, err := amqp.Dial(connStr)
 	if err != nil {
-		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+		log.Fatalf("failed to connect to RabbitMQ: %v", err)
 	}
 	defer conn.Close()
-	fmt.Println("Successfully connected to RabbitMQ!")
+	fmt.Println("successfully connected to RabbitMQ!")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("failed to open AMQP channel: %v", err)
+	}
+
+	//publish message to exchange
+	err = pubsub.PublishJSON(ch,)
 
 	//wait for control+c
 	signalChan := make(chan os.Signal, 1)
