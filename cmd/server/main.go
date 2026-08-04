5a81ae428d9dc7e03e81667d7b768675e825203d
luckyBambooBro <huypham008@hotmail.com>
2026-08-05 06:58:20 +0930

    3.5 q5

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 
@@ -27,14 +28,46 @@ func main() {
 		log.Fatalf("failed to open AMQP channel: %v", err)
 	}
 
-	//publish message to exchange
-	err = pubsub.PublishJSON(ch, 
-		routing.ExchangePerilDirect, 
-		routing.PauseKey, 
-		routing.PlayingState {IsPaused: true},
-	)
-	if err != nil {
-		log.Fatalf("unable to publish playing state: %v", err)
+	gamelogic.PrintServerHelp()
+	for {
+		inputWords := gamelogic.GetInput()
+		if len(inputWords) == 0 {
+			continue
+		}
+
+		switch inputWords[0] {
+		case "pause":
+				//publish message to exchange
+			err = pubsub.PublishJSON(ch, 
+				routing.ExchangePerilDirect, 
+				routing.PauseKey, 
+				routing.PlayingState {IsPaused: true},
+			)
+			if err != nil {
+				log.Fatalf("unable to publish playing state: %v", err)
+			}
+			fmt.Println("Pause message sent!")	
+		
+		case "resume":
+			pubsub.PublishJSON(
+				ch, 
+				routing.ExchangePerilDirect, 
+				routing.PauseKey,
+				routing.PlayingState {IsPaused: false}, 
+			)
+
+		case "quit":
+			log.Println("Exiting Peril")
+			//unsure if return is the correct way to break out of the loop here. but typing "break" just by itself
+			// only breaks out of the switch statement not the outer loop. there is an alternative gemini suggested
+			// which is to label the loop (x) and use break followed by x to break out of loop
+			return
+		
+		default:
+			log.Println("Unknown user command")
+		}
 	}
-	fmt.Println("Pause message sent!")	
+
+
+
 }
