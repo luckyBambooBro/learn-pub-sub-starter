be28a0a1e3b616ca6e075c8c6aa6db77e3722970
luckyBambooBro <huypham008@hotmail.com>
2026-08-23 17:35:08 +0930

    started 6.1

--- internal/pubsub/publish.go
+++ internal/pubsub/publish.go
@@ -1,7 +1,9 @@
 package pubsub
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
 	"encoding/json"
 	"fmt"
 
@@ -27,4 +29,24 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		},
 	)
 
+}
+
+func PublishGob[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	data, err := func (payload T) ([]byte, error) {
+		var network bytes.Buffer
+		enc := gob.NewEncoder(&network)
+		err := enc.Encode(payload)
+		if err != nil {
+			return nil, err
+		}
+		return network.Bytes(), nil
+	}(val)
+
+	if err != nil {
+		fmt.Printf("unable to encode to gob: %v", err)
+		return err
+	}
+
+
+	return nil
 }
